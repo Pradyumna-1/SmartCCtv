6bba9c610338ad1c5ccd8c435c91c4d0cc0702db
Pradyumna-1 <pradyumnakumar35860@gmail.com>
2023-03-19 00:33:52 +0530

    Initial commit

---
+++ just_for_test.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc,20.0,(640,480))
+
+while True:
+    _, frame = cap.read()
+
+    out.write(frame)
+
+    cv2.imshow("", frame)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break 
+   
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+tton(frame1, text='Monitor', height=90, width=180, fg='green',command = find_motion, image=btn1_image, compound='left')
+# btn1['font'] = btn_font
+# btn1.grid(row=3, pady=(20,10))
